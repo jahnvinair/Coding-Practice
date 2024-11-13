adb280a7e6e463b50f77c7f2bb3b23be68c9036d
jahnvinair <141730370+jahnvinair@users.noreply.github.com>
2024-11-13 05:34:34 +0530

    Create mergesort.cpp

---
+++ cpp_code/mergesort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+void merge(int arr[], int p, int q, int r) {
+  int n1 = q - p + 1;
+  int n2 = r - q;
+  int L[n1], M[n2];
+  for (int i = 0; i < n1; i++)
+  L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+  M[j] = arr[q + 1 + j];
+  int i = 0, j = 0, k = p;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int arr[], int left, int right) {
+  if (left < right) {
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+  }
+}
+
+void printArray(const int arr[], int size) {
+  for (int i = 0; i < size; i++)
+  cout << arr[i] << " ";
+  cout << "\n";
+}
+int main() {
+int arr[] = {12, 11, 13, 5, 6, 7};
+int n = sizeof(arr) / sizeof(arr[0]);
+cout << "Given array is \n";
+printArray(arr, n);
+mergeSort(arr, 0, n - 1);
+cout << "\nSorted array is \n";
+printArray(arr, n);
+return 0;
+}
